f75dfbaf3e0842489aa8bd2b65b206b5f41bd492
Sepr0n2 <turpansaid@gmail.com>
2025-05-28 22:42:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("bir cümle yaz");
+        string cümle = Console.ReadLine();
+
+        string[] kelimeler = cümle.Split(new char[] { ' ' });
+        System.Console.WriteLine("Kelime Sayısı:");
+        System.Console.WriteLine(kelimeler.Length);
+
+        int harfs = 0;
+        foreach (var harf in cümle)
+        {
+            if (char.IsLetter(harf))
+            {
+                harfs++;
+            }
+        }
+          Console.WriteLine("Harf Sayısı:");
+          Console.WriteLine(harfs);
+    }
+}
